5d0468611f79f363543ef3e6535e6a9ddc10aed6
Abdelrahman <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2022-08-21 07:24:56 +0200

    create ui structure

--- CoursTech.WebUI/CoursTech/src/app/app.module.ts
+++ CoursTech.WebUI/CoursTech/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/views/home/home.component';
+import { NavbarComponent } from './components/partial-views/navbar/navbar.component';
+import { FooterComponent } from './components/partial-views/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

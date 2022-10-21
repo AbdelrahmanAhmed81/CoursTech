64f2204f36a5b8b0f442bf4064f2af9c92532f18
Abdelrahman <97863124+AbdelrahmanAhmed81@users.noreply.github.com>
2022-10-21 14:43:19 +0200

    adjust courses readmore link

--- CoursTech.WebUI/CoursTech/src/app/directives/readmore.directive.ts
+++ CoursTech.WebUI/CoursTech/src/app/directives/readmore.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, HostBinding, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Directive({
   selector: '[app-Readmore]'
@@ -6,21 +7,27 @@ import { Directive, ElementRef, HostBinding, Input, OnInit } from '@angular/core
 export class ReadmoreDirective implements OnInit {
   @Input() wordsNumber: number = 25;
   @Input() endingString: string = '...';
-  // @HostBinding('value') text: string | undefined;
+  @Input() readmoreNavigateCommands: string[] = [];
 
-  constructor(private elementRef: ElementRef) { }
+  constructor(private elementRef: ElementRef, private router: Router) { }
   ngOnInit(): void {
-    // let readmoreElement:HTMLAnchorElement= document.createElement('a');
-    // debugger;
-    let text = this.elementRef.nativeElement.innerText;
+    let element = this.elementRef.nativeElement;
+
+    let text = element.innerText;
     let words = text.split(' ');
     if (this.wordsNumber < words.length) {
-      this.elementRef.nativeElement.innerText = words.slice(0, this.wordsNumber - 1).join(' ') + this.endingString;
-      // this.elementRef.nativeElement.removeChild(this.elementRef.nativeElement.querySelectorAll('br'))
-      this.elementRef.nativeElement.insertAdjacentHTML('beforeend',
-        `<a style="font-size: smaller;cursor:pointer" class="fw-bold" [routerLink]="['/Course', course.courseId]">
-          read more
-        </a>`);
+      element.innerText = words.slice(0, this.wordsNumber - 1).join(' ') + this.endingString;
+
+      let readmoreLink = document.createElement('a');
+      readmoreLink.innerText = 'read more'
+      readmoreLink.style.fontSize = 'smaller';
+      readmoreLink.style.cursor = 'pointer';
+      readmoreLink.classList.add('fw-bold');
+      readmoreLink.addEventListener('click', () => {
+        this.router.navigate(this.readmoreNavigateCommands)
+      })
+
+      element.appendChild(readmoreLink);
     }
 
   }
